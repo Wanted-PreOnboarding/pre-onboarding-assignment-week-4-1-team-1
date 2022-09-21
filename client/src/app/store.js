1558c14854d8e6e54d5536ebe17eea7550ec196e
unsnruu <unsnruu@gmail.com>
2022-09-21 19:11:40 +0900

    [#9] feat : store 설정
    services의 api를 활용해서 store의 설정을 하였습니다.

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { prefaceApi } from '../services';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [prefaceApi.reducerPath]: prefaceApi.reducer,
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(prefaceApi.middleware),
 });
+
+setupListeners(store.dispatch);
--- client/src/components/Sample.jsx
+++ client/src/components/Sample.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { blueGrey } from '@mui/material/colors';
 
+import { useGetUsersQuery, useLoginUserQuery } from '../services';
+
+const user = {
+  email: 'johndoe2@gmail.com',
+  password: '12345678',
+};
+
 function Sample() {
+  const { data } = useLoginUserQuery(user);
+  console.log(data);
   return (
     <Box sx={{ backgroundColor: blueGrey[200] }}>
       <h1>Sample</h1>
