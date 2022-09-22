9bda9fe08f5b77ba8facb5f63763624c7f2e0fcb
unsnruu <unsnruu@gmail.com>
2022-09-22 17:12:25 +0900

    [#13] feat : RTK Query를 이용한 Api 구성
    RTK Query를 이용하여 Api를 구성했습니다.
    services/users.js는 index.js로 통합하여 삭제하였습니다.

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -29,18 +29,73 @@ export const prefaceApi = createApi({
         body: user,
       }),
     }),
-    getUsers: builder.query({
-      query: () => ({
-        url: 'users',
+    getUsersByPage: builder.query({
+      query: page => ({
+        url: `users/${page}`,
       }),
     }),
-    getAccounts: builder.query({
-      query: () => ({
-        url: 'accounts',
+    searchUser: builder.query({
+      query: word => ({
+        url: `users?q=${word}`,
+      }),
+    }),
+    getFilterdUsers: builder.query({
+      async queryFn({ page = 1, isActive, isStaff }, _queryApi, _extraOptions, fetchWithBQ) {
+        let filterQuery = ``;
+
+        if (isActive === undefined && isStaff === undefined) {
+          const { data } = await fetchWithBQ(`users?_page=${page}`);
+          return { data };
+        }
+        if (isActive !== undefined) filterQuery += `&is_active=${isActive}`;
+        if (isStaff !== undefined) filterQuery += `&is_staff=${isStaff}`;
+
+        const { data: userSettings } = await fetchWithBQ(
+          `userSetting?&_page=${page}${filterQuery}`
+        );
+        const { data: users } = await fetchWithBQ('users');
+
+        const results = [];
+        userSettings.forEach(setting => {
+          users.forEach(user => {
+            if (setting.uuid === user.uuid) {
+              results.push(user);
+            }
+          });
+        });
+        return { data: results };
+      },
+    }),
+    createUser: builder.query({
+      query: newUser => ({
+        url: 'users',
+        method: 'POST',
+        body: newUser,
+      }),
+    }),
+    deleteUser: builder.query({
+      query: id => ({
+        url: `users/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+    editUser: builder.query({
+      query: (id, newUser) => ({
+        url: `users/${id}`,
+        method: 'PATCH',
+        body: newUser,
       }),
     }),
   }),
 });
 
-export const { useLoginUserQuery, useGetUsersQuery, useSignupUserQuery, useGetAccountsQuery } =
-  prefaceApi;
+export const {
+  useLoginUserQuery,
+  useSignupUserQuery,
+  useCreateUserQuery,
+  useDeleteUserQuery,
+  useEditUserQuery,
+  useSearchUserQuery,
+  useGetFilterdUsersQuery,
+  useGetUsersByPageQuery,
+} = prefaceApi;
