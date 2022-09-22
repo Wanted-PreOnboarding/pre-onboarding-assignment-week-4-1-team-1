058fea7cc502703b7bb1efce603f1c99d084caf7
kunnie-code <kearnyyoon@gmail.com>
2022-09-22 16:52:09 +0900

    [#10] comments : 사용사 상세 사용법 코멘트 작성

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       {getToken() ? (
         <Layout>
           <Routes>
-            사용자 상세 리스트를 확인하기 위한 라우터입니다. 사용시 temp폴더에 있는 api파일에 token
-            변수에 토큰을 넣어서 사용하시면 됩니다.
+            {/* 사용자 상세 리스트를 확인하기 위한 라우터입니다. 사용시 temp폴더에 있는 api파일에 token
+            변수에 토큰을 넣어서 사용하시면 됩니다. */}
             {/* <Route path="/" element={<UserList />} />
           <Route path="/users/details/:id" element={<UserDetails />} /> */}
             <Route path="/" element={<Sample />} />
