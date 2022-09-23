a7fa2d2dbc661d6678923c5a4172e508e11664c5
BaikSeungJeon <tmdwjsofficial@naver.com>
2022-09-23 14:37:43 +0900

    [#16] feat : api 모듈화

--- client/src/api/customers.js
+++ client/src/api/customers.js
@@ -1,20 +1,6 @@
 import instance from './index';
 import customersService from '../services/customers';
 
-// class Customers {
-//   async getCustomersByPage(page) {
-//     const res = await instance.get(`/customers?_page=${page}_limit=10`);
-//     return res;
-//   }
-
-//   async searchCustomers(word) {
-//     const res = await instance.get(`/customers?q=${word}`);
-//     return res;
-//   }
-// }
-
-// export default new Customers();
-
 const getCustomersByPage = (page = 1, filters = {}) =>
   customersService.getCustomersByPage(page, filters);
 
