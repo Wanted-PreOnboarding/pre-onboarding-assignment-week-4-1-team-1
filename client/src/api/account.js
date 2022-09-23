ca5b70c7202d76c7df7919cadfd3cd784da0a8a1
unsnruu <unsnruu@gmail.com>
2022-09-23 13:35:31 +0900

    [#29] feat : 함수 이름 변경
    getAnAccount를 getAnAccountById로 변경하였습니다.

--- client/src/api/account.js
+++ client/src/api/account.js
@@ -4,8 +4,8 @@ import accountService from '../services/account';
 const getAccountsByPage = (page = 1, filters = {}) =>
   accountService.getAccountsByPage(page, filters);
 
-const searchAccounts = word => instance.get(`accounts/?q${word}`);
+const searchAccounts = word => instance.get(`accounts/?q=${word}`);
 
-const getAnAccount = id => instance.get(`accounts/${id}`);
+const getAnAccountById = id => instance.get(`accounts/${id}`);
 
-export { getAccountsByPage, searchAccounts, getAnAccount };
+export { getAccountsByPage, searchAccounts, getAnAccountById };
