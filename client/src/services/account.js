a3acc2cb58d8672f8fb1a89a7340b645b1cb9ec0
unsnruu <unsnruu@gmail.com>
2022-09-23 13:01:48 +0900

    [#29] feat : account.service.js에서 account로 이름 변경
    컨벤션에 맞춰 account.service.js에서 account로 이름을 변경하였습니다.

--- client/src/services/account.js
+++ client/src/services/account.js
@@ -1,20 +1,16 @@
-import axiosBase from '../api';
+import instance from '../api';
 import { getToken } from '../utils/token';
 
 class AccountService {
   constructor() {
     this._limit = 10;
     this._accounts = null;
-    this._fetch = axiosBase;
+    this._fetch = instance;
   }
 
   async init() {
     if (!this._accounts) {
-      const { data } = await this._fetch.get('/accounts', {
-        headers: {
-          authorization: `Bearer ${getToken()}`,
-        },
-      });
+      const { data } = await this._fetch.get('/accounts');
       this._accounts = data;
     }
   }
@@ -29,26 +25,21 @@ class AccountService {
     if (brokerName) query += `&broker_name=${brokerName}`;
     if (status) query += `&status=${status}`;
 
-    const { data, headers } = await this._fetch.get(query, {
-      headers: {
-        authorization: `Bearer ${getToken()}`,
-      },
-    });
+    const { data, headers } = await this._fetch.get(query, {});
     const totalLength = headers['x-total-count'];
+
     return { data, meta: { totalLength } };
   }
 
   async searchAccounts(word) {
-    const { data } = await this._fetch.get(`/accounts?q=${word}`, {
-      headers: { authorization: `Bearer ${getToken()}` },
-    });
+    const { data } = await this._fetch.get(`/accounts?q=${word}`, {});
+    console.log(data);
     return data;
   }
 
   async getAnAccount(id) {
-    const { data } = await this._fetch.get(`/accounts/${id}`, {
-      headers: { authorization: `Bearer ${getToken()}` },
-    });
+    const { data } = await this._fetch.get(`/accounts/${id}`, {});
+
     return data;
   }
 }
