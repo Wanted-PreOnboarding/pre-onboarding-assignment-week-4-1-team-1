6cac117c18fdfcbc3df85ebeceff57ac994483d3
unsnruu <unsnruu@gmail.com>
2022-09-23 13:31:36 +0900

    [#29] fix : brokerName에서 brokerID로 수정
    brokerName을 통해 쿼리를 형성하던 부분을
    brokerID를 이용해 수정하는 부분으로 교체했습니다.

--- client/src/services/account.js
+++ client/src/services/account.js
@@ -16,15 +16,15 @@ class AccountService {
 
   async getAccountsByPage(page, filters) {
     this.init();
-
+    //? 브로커 네임을 어떻게 처리할 것인가.
     let query = `accounts?_page=${page}&_limit=${this._limit}`;
-    const { isActive, brokerName, status } = filters;
+    const { isActive, brokerId, status } = filters;
 
     if (isActive) query += `&is_acitve=${isActive}`;
-    if (brokerName) query += `&broker_name=${brokerName}`;
+    if (brokerId) query += `&broker_id=${brokerId}`;
     if (status) query += `&status=${status}`;
 
-    const { data, headers } = await this._fetch.get(query, {});
+    const { data, headers } = await this._fetch.get(query);
     const totalLength = headers['x-total-count'];
 
     return { data, meta: { totalLength } };
