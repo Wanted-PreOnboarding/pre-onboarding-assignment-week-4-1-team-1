becb012cfff7a5c9eb82c8fbdee753f6be0fe453
gomgun <zxolqs@gmail.com>
2022-09-24 19:47:58 +0900

    [#7] feat : 출력 format 관련 util 함수 추가

---
+++ client/src/utils/formating.js
@@ -0,0 +1,15 @@
+export const getAssetPattern = (number) => {
+  return Math.floor(number).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+export function getSimpleDatePattern(date) {
+  const formattedDate = new Date(date);
+
+  const yyyy = formattedDate.getFullYear();
+  const mm = formattedDate.getMonth();
+  const dd = formattedDate.getDate();
+
+  const transferedDate = yyyy + '년 ' + (mm + 1) + '월 ' + dd + '일';
+
+  return transferedDate;
+}
